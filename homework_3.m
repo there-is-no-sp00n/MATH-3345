6e19c38d11112320d731ace2ca8c54f254430878
there-is-no-sp00n <22650592+there-is-no-sp00n@users.noreply.github.com>
2017-09-20 23:15:09 -0500

    Update homework_3.m

--- homework_3.m
+++ homework_3.m
@@ -1,3 +1,16 @@
+function tada = tay(x,n)
+
+for k=1:n
+  y(:, k+1) = x^(k+1)/factorial(k+1);
+end
+tada = 1 + sum(y);
+
+end
+
 x = linspace(-1,1,50);
 
-n = 4;
+j = tay(x, 4);
+
+disp(j);
+
+plot(x,j);
