ab1898af4f6cccae08bd6c61830647bc8419849e
there-is-no-sp00n <22650592+there-is-no-sp00n@users.noreply.github.com>
2017-09-21 06:17:30 -0500

    Update homework_3.m

--- homework_3.m
+++ homework_3.m
@@ -1,23 +1,36 @@
 function tada = tay(x,n)
 
+y=zeros;
 for k=1:n
-  y(:, k) = x.^(k)./factorial(k);
+  y = y + (x.^(k)./factorial(k));
 end
 
+disp('this is y');
+disp(y);
+
+disp('this is sum or y and then sum(y) + 1')
 disp(sum(y));
 disp(1+sum(y));
-int c = sum(y);
-c += 1;
+
+c = sum(y);
+c = c.+size(y);
+
+disp('this is c');
 disp(c);
-tada = c;
+
+tada = c(2);
 
 end
 
-x = linspace(-1,1,50);
+x = linspace(1,4,4);
+disp(x);
 
-b = 2;
-j = tay(x, b);
+b = 3;
+
+for i=1:50
+  j = tay(x, b);
+end
 
 disp(j);
 
-%plot(x,j);
+plot(x,j);
