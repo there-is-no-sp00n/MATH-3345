b00f79d1cf013ce481ac19a1ae60eeac18f85a1b
there-is-no-sp00n <22650592+there-is-no-sp00n@users.noreply.github.com>
2017-09-21 05:27:24 -0500

    Update homework_3.m

--- homework_3.m
+++ homework_3.m
@@ -1,16 +1,23 @@
 function tada = tay(x,n)
 
 for k=1:n
-  y(:, k+1) = x^(k+1)/factorial(k+1);
+  y(:, k) = x.^(k)./factorial(k);
 end
-tada = 1 + sum(y);
+
+disp(sum(y));
+disp(1+sum(y));
+int c = sum(y);
+c += 1;
+disp(c);
+tada = c;
 
 end
 
 x = linspace(-1,1,50);
 
-j = tay(x, 4);
+b = 2;
+j = tay(x, b);
 
 disp(j);
 
-plot(x,j);
+%plot(x,j);
