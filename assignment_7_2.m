c29ffa07defdc973a70329b28d08257788ef2936
there-is-no-sp00n <22650592+there-is-no-sp00n@users.noreply.github.com>
2017-10-19 00:31:41 -0500

    Update assignment_7_2.m

--- assignment_7_2.m
+++ assignment_7_2.m
@@ -2,25 +2,32 @@
 
 %put the puction in
 f = @b;
+g = @b_prime;
+h = @sec;
 
-%have x values
-x = linspace(-50, 50, 100);
+i = sec(2);
 
-%get the y values
-for k = 1:100;
-y(k) = f(x(k));
-end
-
-plot(x,y);
-
-%l= f(1);
-
-%disp(l);
+disp(i);
 
 
 %function for x^3 - 3x + 1
 function a = b(x)
-a = x^3;
-a = a -(3*x);
-a = a + 1;
+  a = x^3;
+  a = a -(3*x);
+  a = a + 1;
+end
+
+function a = b_prime(x)
+  a = 3*x^2;
+  a = a +3;
+end
+
+function a = sec(x)
+  tolerance = b(x);
+
+  while abs(tolerance > 0.001)
+    x = x - (b(x)/b_prime(x));
+    tolerance = b(x);
+  end
+
 end
