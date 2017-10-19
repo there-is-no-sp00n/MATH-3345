e88b6192d1c16e25c5cec393f6876ad57b80cec1
there-is-no-sp00n <22650592+there-is-no-sp00n@users.noreply.github.com>
2017-10-18 20:29:07 -0500

    Update assignment_7_1.m

--- assignment_7_1.m
+++ assignment_7_1.m
@@ -1,29 +1,25 @@
 %section 3.1 C #18
 
 %create a 100 values between the domain 0 <= x <= pi/2
-x = linspace(0, pi/2, 100)
-  
-%disp(x);
+x = linspace(0, pi/2, 100);
 
 %get the cosine values of each x
 for k = 1:100;
   y(k) = cos(x(k));
 end
 
-%disp(y);
 
 %plot(x,y);
 
 %get the product of each element * the corresponding element
 z = x.*y;
 
-%disp(z);
+
 
 %b contains the index of the largest element in z
 [a,b] = max(z);
 
-%disp(a);
-%disp(b);
+
 
 %get the values from x and cos(x) that give me the largest index
 x_max = x(b);
@@ -32,13 +28,16 @@ y_max = y(b);
 %this should be the same as max(z)
 xy = y_max * x_max;
 
-%disp(xy);
 
-plot(0,0)
-hold on
-plot(x_max,0)
-plot(x_max, y_max)
+
+hold on;
+plot(0,0);
+plot(x_max,0);
+plot(x_max, y_max);
 plot(0, y_max);
-hold off
+hold off;
 
 disp(x_max);
+disp(y_max);
+disp(xy);
+disp(max(z));
