081b824630ceac5b284ff3795cc2cb7f302afc0c
there-is-no-sp00n <22650592+there-is-no-sp00n@users.noreply.github.com>
2017-10-18 21:30:33 -0500

    Update assignment_7_2.m

--- assignment_7_2.m
+++ assignment_7_2.m
@@ -1,13 +1,24 @@
 %section 3.3 C #6a
 
+%put the puction in
 f = @b;
 
-l= f(1);
+%have x values
+x = linspace(-50, 50, 100);
 
-disp(l);
+%get the y values
+for k = 1:100;
+y(k) = f(x(k));
+end
+
+plot(x,y);
+
+%l= f(1);
+
+%disp(l);
 
 
-
+%function for x^3 - 3x + 1
 function a = b(x)
 a = x^3;
 a = a -(3*x);
