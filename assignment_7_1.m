f88b57d5bd4fd8daaf477b4aa71153bc94deba6e
there-is-no-sp00n <22650592+there-is-no-sp00n@users.noreply.github.com>
2017-10-18 19:56:29 -0500

    Update assignment_7_1.m

--- assignment_7_1.m
+++ assignment_7_1.m
@@ -1,5 +1,18 @@
 %section 3.1 C #18
 
-x = linspace(0, pi/2)
+x = linspace(0, pi/2, 100)
   
-show(x)
+disp(x);
+
+for k = 1:100;
+  y(k) = cos(x(k));
+end
+
+disp(y);
+
+plot(x,y);
+
+
+m = y(2);
+
+disp(m);
